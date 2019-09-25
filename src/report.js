09c663c407df02420414821b7cacde37d83c2995
Mark Schroering <mark.schroering@lifeomic.com>
2019-09-25 11:36:30 -0400

    Create FHIR DiagnosticReport resource for pdf report

---
+++ src/report.js
@@ -0,0 +1,51 @@
+
+/* eslint camelcase: "off" */
+const fs = require('fs');
+
+module.exports = async (logger, project, patient, fileUrl, sequenceDate, output) => {
+  const stream = fs.createWriteStream(output, { flags: 'a+' });
+
+  const report = {
+    'resourceType': 'DiagnosticReport',
+    'meta': {
+      'tag': [
+        {
+          'system': 'http://lifeomic.com/fhir/dataset',
+          'code': project
+        }, {
+          code: 'LifeOmic Task Service',
+          system: 'http://lifeomic.com/fhir/source'
+        }, {
+          code: 'Ashion',
+          system: 'http://lifeomic.com/fhir/report-source'
+        }
+      ]
+    },
+    'status': 'final',
+    'code': {
+      'text': 'Ashion GEM ExTra'
+    },
+    'effectiveDateTime': sequenceDate,
+    'subject': {
+      'reference': `Patient/${patient}`
+    },
+    'result': [],
+    'report': {
+      'presentedForm': [{
+        'url': fileUrl,
+        'contentType': 'application/pdf',
+        'title': 'Ashion GEM ExTra'
+      }]
+    }
+  };
+
+  stream.write(`${JSON.stringify(report)}\n`);
+  logger.info(report, 'Saved DiagnosticReport');
+
+  stream.close();
+
+  return new Promise((resolve, reject) => {
+    stream.on('error', reject);
+    stream.on('finish', resolve);
+  });
+};
---
+++ test/unit/report.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const test = require('ava');
+const report = require('../../src/report');
+
+test.beforeEach(async t => {
+  try {
+    fs.unlinkSync(path.join(__dirname, 'report.json'));
+  } catch (ignored) {}
+});
+
+test.serial('The report function will save a DiagnosticReport FHIR resource', async t => {
+  await report(console, 'project', 'patient', 'https://api.com/v1/files/1234', '2000-01-01', path.join(__dirname, 'report.json'));
+
+  const result = fs.readFileSync(path.join(__dirname, 'report.json'), 'utf8');
+  t.is(result,
+    `{"resourceType":"DiagnosticReport","meta":{"tag":[{"system":"http://lifeomic.com/fhir/dataset","code":"project"},{"code":"LifeOmic Task Service","system":"http://lifeomic.com/fhir/source"},{"code":"Ashion","system":"http://lifeomic.com/fhir/report-source"}]},"status":"final","code":{"text":"Ashion GEM ExTra"},"effectiveDateTime":"2000-01-01","subject":{"reference":"Patient/patient"},"result":[],"report":{"presentedForm":[{"url":"https://api.com/v1/files/1234","contentType":"application/pdf","title":"Ashion GEM ExTra"}]}}
+`);
+});
