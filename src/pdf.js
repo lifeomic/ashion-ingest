ed9ae5baed3f17e54ebae322464a5f5a170177cb
Ryan <ryan.ratcliff@lifeomic.com>
2021-02-17 06:42:25 -0500

    CR-830: New PDF parser to catch missing spaces

--- src/pdf.js
+++ src/pdf.js
@@ -1,56 +1,103 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
+const PDFParser = require('pdf2json');
+const pdfParser = new PDFParser();
 
-module.exports = async (logger, input) => {
-  const buffer = fs.readFileSync(input);
-  const patientInfo = {};
-  let diagnosis = null;
-  const data = await pdf(buffer, { max: 1 });
+const TEXT_ARRAY = 'R';
+const TEXT_INDEX = 'T';
+const LINE_HEIGHT_POSITION = 'y';
 
-  for (const line of data.text.split('\n')) {
-    const diagnosisLine = line.match(/^Diagnosis:(.*)/);
-    if (diagnosisLine) {
-      diagnosis = diagnosisLine[1].trim();
-    }
+const PATIENT_TAG = encodeURIComponent('Patient:');
+const DIAGNOSIS_TAG = encodeURIComponent('Diagnosis:');
+const DATE_TAG = encodeURIComponent('Date:');
+const DOB_TAG = encodeURIComponent('DOB:');
+const SITE_TAG = encodeURIComponent('Site:');
+const NUMBER_TAG = encodeURIComponent('#:');
+const GENDER_TAG = encodeURIComponent('Gender:');
+const REPORT_TAG = 'Report';
+const SPECIMEN_TAG = 'Specimen';
+const MEDICAL_TAG = 'Medical';
+const RECORD_TAG = 'Record';
 
-    const reportLine = line.match(/^Report\s*Date:(.*)/);
-    if (reportLine) {
-      patientInfo.indexedDate = reportLine[1].trim();
-    }
-    const nameLine = line.match(/Patient:(.*)Ordering\s*Client:(.*)/);
-    if (nameLine) {
-      const [first, last] = nameLine[1].trim().split(' ');
-      patientInfo.firstName = first.trim();
-      patientInfo.lastName = last.trim();
-    }
-    const dobSpecLine = line.match(/DOB:(.*)Specimen\s*Site:(.*)/);
-    if (dobSpecLine) {
-      patientInfo.dob = dobSpecLine[1].trim();
-      patientInfo.bodySite = dobSpecLine[2].trim().replace(/([a-zA-Z])(?=[A-Z])/g, '$1 ');
-    }
-
-    const dobLine = line.match(/DOB:(.*)/);
-    if (dobLine && !patientInfo.dob) {
-      patientInfo.dob = dobLine[1].trim();
-    }
-
-    const specLine = line.match(/Specimen\s*Site:(.*)/);
-    if (specLine && !patientInfo.bodySite) {
-      patientInfo.bodySite = specLine[1].trim().replace(/([a-zA-Z])(?=[A-Z])/g, '$1 ');
-    }
-
-    const mrnLine = line.match(/Medical\s*Record\s*#:(.*)Tumor\s*Collection\s*Date:(.*)/) ||
-          line.match(/Medical\s*Record\s*#:(.*)/);
-    if (mrnLine) {
-      patientInfo.mrn = mrnLine[1].trim();
-    }
-    const genderLine = line.match(/Gender:(.*)Specimen\s*(Type|Site):(.*)/);
-    if (genderLine) {
-      patientInfo.gender = genderLine[1].trim();
-    }
+function extractAllTextOnLineAfterIndex (
+  page1TextFields,
+  lineHeight,
+  currentIndex
+) {
+  const result = [];
+  // adding one to the initial index to step pass the tag field
+  for (
+    currentIndex++;
+    lineHeight === page1TextFields[currentIndex][LINE_HEIGHT_POSITION];
+    currentIndex++
+  ) {
+    result.push(page1TextFields[currentIndex][TEXT_ARRAY][0][TEXT_INDEX]);
   }
 
-  logger.info({input}, 'Process patient in from pdf report file');
+  return result.join(' ');
+}
 
+function parsePdf (input) {
+  return new Promise(function (resolve, reject) {
+    pdfParser.on('pdfParser_dataError', (errData) => reject(errData));
+    pdfParser.on('pdfParser_dataReady', (pdfData) => {
+      const patientInfo = {};
+      let diagnosis = null;
+
+      // we only need the first page so lets narrow down our json
+      const page1TextFields = pdfData['formImage']['Pages'][0]['Texts'];
+      for (let i = 0; i < page1TextFields.length; i++) {
+        const field = page1TextFields[i];
+        if (field[TEXT_ARRAY][0][TEXT_INDEX] === PATIENT_TAG) {
+          patientInfo.firstName =
+            page1TextFields[i + 1][TEXT_ARRAY][0][TEXT_INDEX];
+          patientInfo.lastName =
+            page1TextFields[i + 2][TEXT_ARRAY][0][TEXT_INDEX];
+        } else if (field[TEXT_ARRAY][0][TEXT_INDEX] === DIAGNOSIS_TAG) {
+          diagnosis = extractAllTextOnLineAfterIndex(
+            page1TextFields,
+            field[LINE_HEIGHT_POSITION],
+            i
+          );
+        } else if (
+          field[TEXT_ARRAY][0][TEXT_INDEX] === REPORT_TAG &&
+          page1TextFields[i + 1][TEXT_ARRAY][0][TEXT_INDEX] === DATE_TAG
+        ) {
+          patientInfo.indexedDate = decodeURIComponent(
+            page1TextFields[i + 2][TEXT_ARRAY][0][TEXT_INDEX]
+          );
+        } else if (field[TEXT_ARRAY][0][TEXT_INDEX] === DOB_TAG) {
+          patientInfo.dob = decodeURIComponent(
+            page1TextFields[i + 1][TEXT_ARRAY][0][TEXT_INDEX]
+          );
+        } else if (
+          field[TEXT_ARRAY][0][TEXT_INDEX] === SPECIMEN_TAG &&
+          page1TextFields[i + 1][TEXT_ARRAY][0][TEXT_INDEX] === SITE_TAG
+        ) {
+          patientInfo.bodySite = extractAllTextOnLineAfterIndex(
+            page1TextFields,
+            field[LINE_HEIGHT_POSITION],
+            i + 1
+          );
+        } else if (
+          field[TEXT_ARRAY][0][TEXT_INDEX] === MEDICAL_TAG &&
+          page1TextFields[i + 1][TEXT_ARRAY][0][TEXT_INDEX] === RECORD_TAG &&
+          page1TextFields[i + 2][TEXT_ARRAY][0][TEXT_INDEX] === NUMBER_TAG
+        ) {
+          patientInfo.mrn = page1TextFields[i + 3][TEXT_ARRAY][0][TEXT_INDEX];
+        } else if (field[TEXT_ARRAY][0][TEXT_INDEX] === GENDER_TAG) {
+          patientInfo.gender =
+            page1TextFields[i + 1][TEXT_ARRAY][0][TEXT_INDEX];
+        }
+      }
+
+      return resolve({diagnosis, patientInfo});
+    });
+
+    pdfParser.loadPDF(input);
+  });
+}
+
+module.exports = async (logger, input) => {
+  const {diagnosis, patientInfo} = await parsePdf(input);
+  logger.info({input}, 'Process patient in from pdf report file');
   return { diagnosis, patientInfo };
 };
